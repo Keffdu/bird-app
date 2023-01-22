8e33f46bba24f90545521e18bcd74a15f0aaa233
Kevin Dutzy <kddutz15@gmail.com>
2023-01-22 13:25:37 -0500

    added show action

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -3,4 +3,11 @@ class BirdsController < ApplicationController
     def index
         render json: Bird.all, status: 200
     end
+
+    def show
+        bird = Bird.find(params[:id])
+        render json: bird, status: 200
+    rescue ActiveRecord::RecordNotFound
+        render json: "Bird not found", status: 404
+    end
 end
